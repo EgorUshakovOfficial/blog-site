4fa93f68e6e65a9546543b12102e78aceb166e9d
Egor Ushakov <egorushakov13@gmail.com>
2022-09-14 14:49:27 -0600

    Changed boostrap Button to button

--- client/src/pages/Blogpost/CommentOptions.js
+++ client/src/pages/Blogpost/CommentOptions.js
@@ -5,17 +5,16 @@ import {
 } from '@fortawesome/free-solid-svg-icons'; 
 import { useContext } from 'react'; 
 import { CommentContext } from '../../context/CommentProvider';
-import Button from 'react-bootstrap/Button'; 
 export default function CommentOptions({commentId}) {
     const { setEditCommentId, setDeleteCommentId} = useContext(CommentContext); 
     return (
         <div className="comment-options">
-            <Button variant="secondary" style={{ marginRight: "0.25em" }} onClick={() => setEditCommentId(commentId)}>
+            <button className="btn btn-secondary" style={{ marginRight: "0.25em" }} onClick={() => setEditCommentId(commentId)}>
                 <FontAwesomeIcon icon={faPen} />
-            </Button>
-            <Button variant="danger" onClick={() => setDeleteCommentId(commentId)}>
+            </button>
+            <button className="btn btn-danger" onClick={() => setDeleteCommentId(commentId)}>
                 <FontAwesomeIcon icon={faTrash} />
-            </Button>
+            </button>
         </div>
     )
 }
