616ac08cb173d803fae306acd3313182c74adc20
Egor Ushakov <egorushakov13@gmail.com>
2022-09-18 20:27:29 -0600

    Fixed bug in server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
         app.use(express.static('client/build'))
 
         app.get("*", (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+            res.sendFile(path.resolve(process.cwd(), 'client', 'build', 'index.html'))
         })
 
     }
